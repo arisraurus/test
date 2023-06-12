169b2d30f147e54241b26a8b1c5600f3327a0b89
Tri Arisma <101231978+arisraurus@users.noreply.github.com>
2023-06-12 20:59:18 +0700

    upload file pertama

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Test</title>
+</head>
+
+<body>
+    <h3>Hallo ini adalah test repository</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit sequi debitis mollitia magnam sapiente, temporibus aspernatur ullam quia veniam id rem, dolorem quaerat incidunt commodi possimus eius, alias ad? Itaque non incidunt obcaecati, dolorum perspiciatis laudantium est rerum sapiente quasi quaerat nisi placeat odio adipisci repellat doloribus accusantium distinctio ad.</p>
+    <p>by Tri Arisma</p>
+</body>
+
+</html>
